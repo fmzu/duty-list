e2a6c73ee83216184f7c1a33956ea863888cd496
fmzu <fjtmzk0723@gmail.com>
2024-10-15 16:40:24 +0900

    refactor

--- api/interface/api.ts
+++ api/interface/api.ts
@@ -4,7 +4,7 @@ import { authConfig } from "~/interface/auth-config"
 import { myUserRoutes } from "~/interface/routes/my-user"
 import { roster } from "~/interface/routes/roster"
 import { tagsRoutes } from "~/interface/routes/tags"
-import { taskRoutes } from "~/interface/routes/task"
+import { taskRoutes } from "~/interface/routes/tasks"
 import { usersRoutes } from "~/interface/routes/users"
 
 export const api = apiFactory
